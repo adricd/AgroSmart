89117dc7ab25398de3fd7cf524d7c48ac5769585
adricd <adricasasdu@gmail.com>
2026-07-04 18:06:19 -0500

    us 36,31 y 32

--- public/assets/scripts/scriptscultivos.js
+++ public/assets/scripts/scriptscultivos.js
@@ -137,3 +137,29 @@ document
     previewFoto.src =
     "imagenes/subir-imagen.png";
 });
+const btnCerrarSesion = document.getElementById("btnCerrarSesion");
+const logoutOverlay = document.getElementById("logoutOverlay");
+const btnCancelarLogout = document.getElementById("btnCancelarLogout");
+const btnAceptarLogout = document.getElementById("btnAceptarLogout");
+
+if(btnCerrarSesion){
+
+    btnCerrarSesion.onclick = function(){
+
+        logoutOverlay.style.display="flex";
+
+    }
+
+    btnCancelarLogout.onclick=function(){
+
+        logoutOverlay.style.display="none";
+
+    }
+
+    btnAceptarLogout.onclick=function(){
+
+        window.location.href="login.html";
+
+    }
+
+} 
